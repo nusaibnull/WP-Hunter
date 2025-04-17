2996d7036accb8fabc2436909424026d834c097f
nusaibnull <73007610+nusaibnull@users.noreply.github.com>
2025-04-17 23:12:10 +0600

    Delete modules directory

--- modules/alive_checker.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def is_site_alive(url, timeout=5):
-    try:
-        res = requests.get(url, timeout=timeout)
-        return res.status_code < 400
-    except:
-        return False
